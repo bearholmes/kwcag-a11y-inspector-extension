992c303644650a3d571b83f2bdd653bb38def33e
bearholmes <bearholmes@gmail.com>
2023-03-17 21:52:53 +0900

    코드 정리

--- js/background.js
+++ js/background.js
@@ -10,47 +10,49 @@
  * This source code is licensed under the GNU General Public License,
  * Version 2. See the file COPYING for more details.
  */
-var dkInspectLoaded = false;
 
 // Check whether new version is installed
-chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason == "install") {
-        chrome.tabs.create( {url: "option.html"} );
-        chrome.storage.local.set({'monitors': '17'});
-        chrome.storage.local.set({'resolutions': '1366x768'});
-        chrome.storage.local.set({'ccshow': '1'});
-        chrome.storage.local.set({'linkmode': '0'});
-        chrome.storage.local.set({'bgmode': '1'});
-        chrome.storage.local.set({'linetype': 'dashed'});
-        chrome.storage.local.set({'colortype': 'ff0000'});
-        chrome.storage.local.set({'bordersize': '2'});
-    }
-});
-// || details.reason == "update" 제외
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  if(request.cmd == "pause"){
-    sendResponse('27'); //esc
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('onInstalled');
+  if (details.reason == 'install') {
+    console.log('install');
+    chrome.tabs.create({ url: 'option.html' });
+    chrome.storage.local.set({
+      monitors: '17',
+      resolutions: '1366x768',
+      ccshow: '1',
+      linkmode: '0',
+      bgmode: '1',
+      linetype: 'dashed',
+      colortype: 'ff0000',
+      bordersize: '2',
+    });
   }
 });
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    if (tab.url.indexOf("https://chrome.google.com") == 0 || tab.url.indexOf("chrome://") == 0) {
-        alert("크롬 내부 페이지에서는 동작하지 않습니다."); //It does not work on Google Chrome Internal pages
-        return;
-    }
-    chrome.tabs.executeScript(tab.id, {file: 'js/dkinspect.js'});
-    chrome.tabs.insertCSS(tab.id, {file: 'css/dkinspect.css'});
-
-    dkInspectLoaded = true;
+chrome.browserAction.onClicked.addListener(function (tab) {
+  if (
+    tab.url.indexOf('https://chrome.google.com') == 0 ||
+    tab.url.indexOf('chrome://') == 0
+  ) {
+    alert('크롬 내부 페이지에서는 동작하지 않습니다.'); //It does not work on Google Chrome Internal pages
+    return;
+  }
+  chrome.tabs.executeScript(tab.id, { file: 'js/dkinspect.js' });
+  chrome.tabs.insertCSS(tab.id, { file: 'css/dkinspect.css' });
 });
 
 chrome.contextMenus.create({
-    contexts: ["browser_action"],
-    title: "수동계산하기",
-    onclick: function(tab) {
-    	  chrome.tabs.executeScript(tab.id, {file: 'js/cals.js'});
-    	    chrome.tabs.insertCSS(tab.id, {file: 'css/cals.css'});
-      }
+  contexts: ['browser_action'],
+  title: '수동계산하기',
+  onclick: function (tab) {
+    chrome.tabs.executeScript(tab.id, { file: 'js/cals.js' });
+    chrome.tabs.insertCSS(tab.id, { file: 'css/cals.css' });
+  },
 });
 
-
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.cmd === 'pause') {
+    sendResponse('27'); //esc
+  }
+});
--- js/option.js
+++ js/option.js
@@ -1,127 +1,125 @@
 /*jshint browser: true */
 /*global chrome */
 
-var $ = function() {
-    return document.getElementById(arguments[0]);
+const $ = function () {
+  return document.getElementById(arguments[0]);
 };
 
 function loadEvent() {
-    chrome.storage.local.get('monitors', function(result) {
-        var monitors = result.monitors;
-        if (monitors) {
-            $(monitors).selected = true;
-        }
-    });
-    chrome.storage.local.get('resolutions', function(result) {
-        var resolutions = result.resolutions;
-        if (resolutions) {
-            $(resolutions).selected = true;
-        }
-    });
-    chrome.storage.local.get('ccshow', function(result) {
-        var ccshow = result.ccshow;
+  chrome.storage.local.get('monitors', function (result) {
+    const monitors = result.monitors;
+    if (monitors) {
+      $(monitors).selected = true;
+    }
+  });
+  chrome.storage.local.get('resolutions', function (result) {
+    const resolutions = result.resolutions;
+    if (resolutions) {
+      $(resolutions).selected = true;
+    }
+  });
+  chrome.storage.local.get('ccshow', function (result) {
+    const ccshow = result.ccshow;
 
-        if (ccshow == 1) {
-            $("ccShowOn").checked  = true;
-        } else {
-        	  $("ccShowOff").checked  = true;
-        }
-    });
-    chrome.storage.local.get('linkmode', function(result) {
-        var linkmode = result.linkmode;
+    if (ccshow === 1) {
+      $('ccShowOn').checked = true;
+    } else {
+      $('ccShowOff').checked = true;
+    }
+  });
+  chrome.storage.local.get('linkmode', function (result) {
+    const linkmode = result.linkmode;
 
-        if (linkmode == 1) {
-            $("linkModeOn").checked  = true;
-        } else {
-          	$("linkModeOff").checked  = true;
-        }
-    });
-    chrome.storage.local.get('bgmode', function(result) {
-        var bgmode = result.bgmode;
+    if (linkmode === 1) {
+      $('linkModeOn').checked = true;
+    } else {
+      $('linkModeOff').checked = true;
+    }
+  });
+  chrome.storage.local.get('bgmode', function (result) {
+    const bgmode = result.bgmode;
 
-        if (bgmode == 1) {
-            $("bgModeOn").checked  = true;
-        } else {
-          	$("bgModeOff").checked  = true;
-        }
-    });
-    chrome.storage.local.get('linetype', function(result) {
-        var linetype = result.linetype;
+    if (bgmode === 1) {
+      $('bgModeOn').checked = true;
+    } else {
+      $('bgModeOff').checked = true;
+    }
+  });
+  chrome.storage.local.get('linetype', function (result) {
+    const linetype = result.linetype;
 
-        if (linetype == "solid") {
-            $("linetype1").checked  = true;
-        } else if (linetype == "dashed") {
-            $("linetype2").checked  = true;
-        } else if (linetype == "dotted") {
-            $("linetype3").checked  = true;
-        }
-    });
-    chrome.storage.local.get('colortype', function(result) {
-        var colortype = result.colortype;
-        $("colorType").value  = colortype;
-    });
-    chrome.storage.local.get('trackingmode', function(result) {
-        var trackingmode = result.trackingmode;
+    if (linetype === 'solid') {
+      $('linetype1').checked = true;
+    } else if (linetype === 'dashed') {
+      $('linetype2').checked = true;
+    } else if (linetype === 'dotted') {
+      $('linetype3').checked = true;
+    }
+  });
+  chrome.storage.local.get('colortype', function (result) {
+    $('colorType').value = result.colortype;
+  });
+  chrome.storage.local.get('trackingmode', function (result) {
+    const trackingmode = result.trackingmode;
 
-        if (trackingmode == "true") {
-            $("trackingModeOn").checked  = true;
-        } else {
-            $("trackingModeOff").checked  = true;
-        }
-    });
-    chrome.storage.local.get('bordersize', function(result) {
-        var bordersize = result.bordersize;
-        $("bordersize").value = bordersize;
-    });
+    if (trackingmode === 'true') {
+      $('trackingModeOn').checked = true;
+    } else {
+      $('trackingModeOff').checked = true;
+    }
+  });
+  chrome.storage.local.get('bordersize', function (result) {
+    $('bordersize').value = result.bordersize;
+  });
 }
 
 function resRegEvent() {
-    var monitor = $("moniStd").value;
-    var resolution = $("resStd").value;
+  const monitor = $('moniStd').value;
+  const resolution = $('resStd').value;
 
-    var cc_sw = 0;
-    if ($("ccShowOn").checked) cc_sw = 1;
+  let cc_sw = 0;
+  if ($('ccShowOn').checked) cc_sw = 1;
 
-    var lm_sw = 0;
-    if ($("linkModeOn").checked) lm_sw = 1;
+  let lm_sw = 0;
+  if ($('linkModeOn').checked) lm_sw = 1;
 
-    var bg_sw = false;
-    if ($("bgModeOn").checked) bg_sw = true;
+  let bg_sw = false;
+  if ($('bgModeOn').checked) bg_sw = true;
 
-    var linetype = "dashed";
-    if($("linetype1").checked){
-      linetype = "solid";
-    } else if ($("linetype2").checked){
-      linetype = "dashed";
-    } else if ($("linetype3").checked){
-      linetype = "dotted";
-    }
-    var colortype = $("colorType").value;
-    
-    var trackingmode = false;
-    if($("trackingModeOn").checked) trackingmode = true;
+  let linetype = 'dashed';
+  if ($('linetype1').checked) {
+    linetype = 'solid';
+  } else if ($('linetype2').checked) {
+    linetype = 'dashed';
+  } else if ($('linetype3').checked) {
+    linetype = 'dotted';
+  }
+  const colortype = $('colorType').value;
 
-    var bordersize = $("bordersize").value;
+  let trackingmode = false;
+  if ($('trackingModeOn').checked) trackingmode = true;
 
-    chrome.storage.local.set({
-        'monitors': monitor,
-        'resolutions': resolution,
-        'ccshow': cc_sw,
-        'linkmode': lm_sw,
-        'bgmode': bg_sw,
-        'linetype': linetype,
-        'colortype': colortype,
-        'trackingmode': trackingmode,
-        'bordersize' : bordersize
-    });
+  const bordersize = $('bordersize').value;
 
-    $("resStatus").innerText = "저장완료!";
-    setTimeout(function(){
-      $("resStatus").innerText ="";
-    }, 8000);
+  chrome.storage.local.set({
+    monitors: monitor,
+    resolutions: resolution,
+    ccshow: cc_sw,
+    linkmode: lm_sw,
+    bgmode: bg_sw,
+    linetype: linetype,
+    colortype: colortype,
+    trackingmode: trackingmode,
+    bordersize: bordersize,
+  });
+
+  $('resStatus').innerText = '저장완료!';
+  setTimeout(function () {
+    $('resStatus').innerText = '';
+  }, 8000);
 }
 
-window.addEventListener('DOMContentLoaded', function() {
-    $("resBtn").addEventListener('click', resRegEvent, false);
-    loadEvent();
+window.addEventListener('DOMContentLoaded', function () {
+  $('resBtn').addEventListener('click', resRegEvent, false);
+  loadEvent();
 });
